3b4df5ce2bf476fecf7d897c5059725d12ba0dbc
Seto Balian <seto.balian@gmail.com>
2021-12-02 11:05:53 +0000

    Day 1

---
+++ day1/day1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+
+def read_report(path):
+    with open(path) as f:
+        report = [int(m) for m in f.read().strip().split("\n")]
+    return report
+
+
+def part1(path):
+    report = read_report(path)
+    inc = 0
+    n = len(report)
+    for i in range(1, n):
+        current = report[i]
+        previous = report[i - 1]
+        if current > previous:
+            inc += 1
+    return inc
+
+
+def part2(path):
+    report = read_report(path)
+    n = len(report)
+    inc = 0
+    for i in range(n - 3):
+        if report[i + 3] > report[i]:
+            inc += 1
+    return inc
+
+
+def i_have_too_much_time_to_kill():
+    # modified from tcbegley solution
+    print(
+        (
+            (ds := list(map(int, open("input.txt").readlines())))
+            and (
+                sum(d1 < d2 for d1, d2 in zip(ds, ds[1:])),
+                sum(ds[i] < ds[i + 3] for i in range(len(ds) - 3)),
+            )
+        )
+    )
+
+
+def main():
+    assert part1("input.txt") == 1400
+    assert part2("input.txt") == 1429
+
+    print("All tests passed.")
+
+
+if __name__ == "__main__":
+    main()
