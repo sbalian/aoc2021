0356665877335b533fa3de79d4a38a46a69eb823
Seto Balian <seto.balian@gmail.com>
2021-12-04 16:21:27 +0000

    Improvements to Day 3

--- day3/day3.py
+++ day3/day3.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
 
-import copy
-import operator
-
-
 def read_report(path):
     with open(path) as f:
         report = f.read().strip().split("\n")
@@ -12,67 +8,51 @@ def read_report(path):
 
 
 def part1(report):
-    num_cols = len(report[0])
-    num_rows = len(report)
-    freq = [{"0": 0, "1": 0} for _ in range(num_cols)]
-    for i in range(num_rows):
-        for j in range(num_cols):
-            if report[i][j] == "0":
-                freq[j]["0"] += 1
+    num_cols, num_rows = len(report[0]), len(report)
+    gamma_rate, epsilon_rate = "", ""
+    for i in range(num_cols):
+        num_zeros, num_ones = 0, 0
+        for j in range(num_rows):
+            if report[j][i] == "0":
+                num_zeros += 1
             else:
-                freq[j]["1"] += 1
-    gamma_rate = int(
-        "".join([max(f.items(), key=operator.itemgetter(1))[0] for f in freq]),
-        2,
-    )
-    epsilon_rate = int(
-        "".join([min(f.items(), key=operator.itemgetter(1))[0] for f in freq]),
-        2,
-    )
-    return gamma_rate * epsilon_rate
+                num_ones += 1
+        if num_ones > num_zeros:
+            gamma_rate += "1"
+            epsilon_rate += "0"
+        else:
+            gamma_rate += "0"
+            epsilon_rate += "1"
+    return int(gamma_rate, 2) * int(epsilon_rate, 2)
+
+
+def _part2(report, rating):
+    if rating not in ["oxygen", "co2"]:
+        raise ValueError("rating must be oxygen or co2")
+    num_cols = len(report[0])
+    for i in range(num_cols):
+        if len(report) == 1:
+            break
+        num_zeros, num_ones = 0, 0
+        for row in report:
+            if row[i] == "1":
+                num_ones += 1
+            else:
+                num_zeros += 1
+        if num_ones >= num_zeros and rating == "oxygen":
+            to_keep = "1"
+        elif num_ones >= num_zeros and rating == "co2":
+            to_keep = "0"
+        elif num_ones < num_zeros and rating == "oxygen":
+            to_keep = "0"
+        else:
+            to_keep = "1"
+        report = [x for x in report if x[i] == to_keep]
+    return int(report[0], 2)
 
 
 def part2(report):
-    oxygen = report
-    co2 = copy.copy(oxygen)
-
-    num_cols = len(report[0])
-
-    for i in range(num_cols):
-        if len(oxygen) == 1:
-            break
-        num_zeros = 0
-        num_ones = 0
-        for row in oxygen:
-            if row[i] == "1":
-                num_ones += 1
-            else:
-                num_zeros += 1
-        if num_ones >= num_zeros:
-            to_keep = "1"
-        else:
-            to_keep = "0"
-        oxygen = [x for x in oxygen if x[i] == to_keep]
-    oxygen = int(oxygen[0], 2)
-
-    for i in range(num_cols):
-        if len(co2) == 1:
-            break
-        num_zeros = 0
-        num_ones = 0
-        for row in co2:
-            if row[i] == "1":
-                num_ones += 1
-            else:
-                num_zeros += 1
-        if num_ones < num_zeros:
-            to_keep = "1"
-        else:
-            to_keep = "0"
-        co2 = [x for x in co2 if x[i] == to_keep]
-    co2 = int(co2[0], 2)
-
-    return oxygen * co2
+    return _part2(report, "oxygen") * _part2(report, "co2")
 
 
 def main():
