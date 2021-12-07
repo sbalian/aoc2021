e9845630d3b7f3554d72ab992d096c9e6c894ca3
Seto Balian <seto.balian@gmail.com>
2021-12-07 21:25:26 +0000

    Minor improvement to Day 7

--- day7/day7.py
+++ day7/day7.py
@@ -6,33 +6,24 @@ def read_positions(path):
         return list(map(int, f.read().strip().split(",")))
 
 
-def part1(positions):
-    fuels = []
-    n = len(positions)
-    for i in range(max(positions)):
-        fuel = []
-        for j in range(n):
-            fuel.append(abs(i - positions[j]))
-        fuels.append(sum(fuel))
-    return min(fuels)
-
-
-def part2(positions):
+def min_cost(positions, part=1):
     fuels = []
     n = len(positions)
     for i in range(max(positions)):
         fuel = []
         for j in range(n):
             diff = abs(i - positions[j])
-            fuel.append((diff / 2) * (diff + 1))
+            if part == 2:
+                diff = (diff / 2) * (diff + 1)
+            fuel.append(diff)
         fuels.append(sum(fuel))
     return min(fuels)
 
 
 def main():
     positions = read_positions("input.txt")
-    assert part1(positions) == 328187
-    assert part2(positions) == 91257582
+    assert min_cost(positions, 1) == 328187
+    assert min_cost(positions, 2) == 91257582
     print("All tests passed.")
 
 
