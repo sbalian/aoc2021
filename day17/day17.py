e6bba00cedf1acdeadb145f5af1f6cd68e3e0c7e
Seto Balian <seto.balian@gmail.com>
2022-07-14 02:48:12 +0100

    Day 17 Part 2

--- day17/day17.py
+++ day17/day17.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 import re
 
 INPUT = "target area: x=88..125, y=-157..-103"
@@ -10,8 +11,8 @@ def get_target_area(input_string):
     return {
         bound: int(value)
         for bound, value in re.match(
-            r"target area: x=(?P<x_min>-\d+|\d+)\.\.(?P<x_max>-\d+|\d+), "
-            r"y=(?P<y_min>-\d+|\d+)\.\.(?P<y_max>-\d+|\d+)",
+            r"target area: x=(?P<min_x>-\d+|\d+)\.\.(?P<max_x>-\d+|\d+), "
+            r"y=(?P<min_y>-\d+|\d+)\.\.(?P<max_y>-\d+|\d+)",
             input_string,
         )
         .groupdict()
@@ -19,9 +20,49 @@ def get_target_area(input_string):
     }
 
 
+def find_maximum_vertical_velocity(target_area):
+    return abs(target_area["min_y"]) - 1
+
+
 def part1(target_area):
-    max_v = abs(target_area["y_min"]) - 1
+    max_v = find_maximum_vertical_velocity(target_area)
     return (max_v * (max_v + 1)) // 2
+
+
+def part2(target_area):
+    max_x = target_area["max_x"]
+    min_x = target_area["min_x"]
+    max_y = target_area["max_y"]
+    min_y = target_area["min_y"]
+
+    min_v_x = math.ceil((-1 + math.sqrt(1 + 8 * min_x)) / 2)
+    max_v_x = max_x
+    min_v_y = min_y
+    max_v_y = find_maximum_vertical_velocity(target_area)
+
+    v_allowed = 0
+
+    for v_x in range(min_v_x, max_v_x + 1):
+        for v_y in range(min_v_y, max_v_y + 1):
+            hit = False
+            x, y = 0, 0
+            v_x_c = v_x
+            v_y_c = v_y
+            while True:
+                if (min_x <= x <= max_x) and (min_y <= y <= max_y):
+                    hit = True
+                    v_allowed += 1
+                    break
+                x += v_x_c
+                y += v_y_c
+                if v_x_c != 0:
+                    v_x_c -= 1
+                v_y_c -= 1
+                if (x > max_x) or (y < min_y):
+                    break
+            if hit:
+                continue
+    return v_allowed
 
 
 def main():
@@ -29,6 +70,9 @@ def main():
     input_target_area = get_target_area(INPUT)
     assert part1(example_target_area) == 45
     assert part1(input_target_area) == 12246
+    assert part2(example_target_area) == 112
+    assert part2(input_target_area) == 3528
+
     print("All tests passed.")
 
 
